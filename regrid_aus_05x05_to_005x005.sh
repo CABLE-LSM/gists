aad786c0ad2f2940b51ca40240916e98e5315557
Sean Bryan <sean.bryan@anu.edu.au>
2024-11-19 11:11:51 +1100

    Set c4frac references attribute

--- regrid_aus_05x05_to_005x005.sh
+++ regrid_aus_05x05_to_005x005.sh
@@ -660,6 +660,14 @@ att_type='c'
 att_val='FRACTIONAL COVER OF C4 GRASS'
 ncatted -h -a "${att_nm}","${var_nm}","${mode}","${att_type}","${att_val}" ${output_file}
 
+echo "Set c4frac references attribute"
+att_nm='references'
+var_nm='c4frac'
+mode='o'
+att_type='c'
+att_val='https://doi.org/10.5194/bg-10-2011-2013 following https://doi.org/10.1007/BF00379569'
+ncatted -h -a "${att_nm}","${var_nm}","${mode}","${att_type}","${att_val}" ${output_file}
+
 bios_soil_vars_regex='^clay$|^sand$|^ssat$|^bch$|^css$|^hyds$|^sfc$|^sucs$|^rhosoil$|^silt$|^swilt$'
 
 echo "Add provenance information to BIOS soil parameters"
